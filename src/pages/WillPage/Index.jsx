4a7a2f6211fcd477946decd9db631581fd0e9207
SahilPanhotra <sahilpanhotra@gmail.com>
2022-11-25 22:12:15 +0530

    style:Navbar padding

--- src/pages/WillPage/Index.jsx
+++ src/pages/WillPage/Index.jsx
@@ -3,7 +3,7 @@ import Card from '../../components/Card/Index'
 
 function WillPage() {
   return (
-    <div className='flex justify-center items-center h-[90vh] max-w-6xl w-4/5 mx-auto my-0 max-[578px]:w-[95%]'>
+    <div className='flex justify-center items-center h-[88vh] max-w-6xl w-4/5 mx-auto my-0 max-[578px]:w-[95%]'>
         <Card/>
     </div>
   )
